ebbcfde8726da72bfe5454445b1f0c8ee70dc853
phoeniXharsh <hv.tiwari178@gmail.com>
2023-04-21 17:29:22 +0530

    improved logic in game

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const rockButton = document.getElementById("#rock");
-const paperButton = document.getElementById('#paper');
-const scissorButton = document.getElementById('#scissor');
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorButton = document.getElementById("scissors");
 const playerScore = document.getElementById('player-score');
 const computerScore = document.getElementById('computer-score');
 const tieScore = document.getElementById('tie-score');
@@ -15,23 +15,21 @@ let tieCount = 0;
 
 // Adding Event Listeners
 function clickListener(event) {
-  // Exit, if not a button was clicked
-  if (event.target.tagName !== "BUTTON") {
-     return;
-  }
-  const playerSelection = event.target.dataset.sign;
-  const computerSelection = computerPlay();
+  const playerSelection = event.target.id;
+  const computerSelection = getComputerChoice();
   playRound(playerSelection,computerSelection);
 }
 
-document.getElementbyId("#button-container").addEventListener("click", clickListener);
+rockButton.addEventListener("click", clickListener);
+paperButton.addEventListener("click", clickListener);
+scissorButton.addEventListener("click", clickListener);
+
 
 
 function getComputerChoice() {
+  let rps = [ROCK, PAPER, SCISSOR];
   let randomNumber = Math.floor(Math.random() * 3);
-  if (randomNumber == 0) return ROCK;
-  else if (randomNumber == 1) return PAPER;
-  else return SCISSOR;
+  return rps[randomNumber];
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -40,39 +38,39 @@ function playRound(playerSelection, computerSelection) {
     (computerSelection == SCISSOR && playerSelection == ROCK) ||
     (computerSelection == PAPER && playerSelection == SCISSOR);
 
+  if (computerSelection == playerSelection) {
+    tie();
+  }
   if (win) {
     playerWin();
   } else {
     computerWin();
   }
-  if (computerSelection == playerSelection) {
-    tie();
-  }
 
-  if(computerWinCount == 5){
-    alert("Better luck next time");
-    reset();
-  }
-  else{
-    alert("Congratulations, You Won!");
-    reset();
-  }
+  // if(computerWinCount == 5){
+  //   alert("Better luck next time");
+  //   reset();
+  // }
+  // else{
+  //   alert("Congratulations, You Won!");
+  //   reset();
+  // }
 
 }
 
 function playerWin(){
   playerWinCount++;
-  playerScore.textContent = "Your score is: " + playerWinCount;
+  playerScore.innerText = "Your score is: " + playerWinCount;
 }
 
 function computerWin(){
   computerWinCount++;
-  computerScore.textContent = "Computer's score is: " + playerWinCount;
+  computerScore.innerText= "Computer's score is: " + computerWinCount;
 }
 
 function tie(){
   tieCount++;
-  playerScore.textContent = "Tie rounds: " + playerWinCount;
+  tieScore.innerText = "Tie rounds: " + tieCount;
 }
 
 function reset() {
